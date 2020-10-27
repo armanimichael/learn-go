ef95190d6b9bb55fd918e06b71f2f9b65327ea65
Michael Armani <michaelarmani00@gmail.com>
2020-10-27 15:51:46 +0100

    Zero types introduction

---
+++ variables/zero-values.go
@@ -0,0 +1,16 @@
+package main
+
+func main() {
+	/*
+		If you don't assign any value to a variable
+		it will return a Zero Value, based on its type.
+		
+		Numeric Types return 0
+		Booleans return false
+		Strings return ""
+	*/
+	var a: int32   // 0
+	var b: float32 // 0
+	var c: bool    // false
+	var d: string  // ""
+}
